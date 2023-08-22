600b7cc65026cdcd5e1a41d6666c59cff7b248d7
predross <igorpedro018@gmail.com>
2023-08-22 14:17:46 -0300

    pronto!

---
+++ lib/book_repository.dart
@@ -0,0 +1,11 @@
+import 'models/book.dart';
+
+final bookRepository = [
+  Book(0, 'Sun tzu', 'desconhecido'),
+  Book(1, 'O País dos Cegos', 'H. G. Wells'),
+  Book(2,'resenha', 'P. Igor'),
+
+
+
+
+];
---
+++ lib/models/book.dart
@@ -0,0 +1,8 @@
+class Book {
+  final int id;
+  final String title;
+  final String author;
+
+  Book(this.id, this.title, this.author);
+
+}
